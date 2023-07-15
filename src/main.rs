858fad1538cf177e34b987f098fb1921cb5a0356
Luiz Henrique Silva Sampaio <luizhsampaio07@gmail.com>
2023-07-15 12:53:08 -0300

    Added mark_position function
    Changes to be committed:
    	modified:   src/main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,21 @@
 fn main() {
-    let board = [["1", "2", "3"], ["4", "5", "6"], ["7", "8", "9"]];
-    write_screen(board);
+    let mut running = true;
+    let mut board = [["1", "2", "3"], ["4", "5", "6"], ["7", "8", "9"]];
+    while running {
+        mark_position(&mut board, "X", 7);
+        write_screen(board);
+        running = false;
+    }
+}
+
+fn mark_position<'a>(board: &mut [[&'a str; 3]; 3], mark: &'a str, position_id: usize) {
+    if position_id <= 3 {
+        board[0][position_id - 1] = mark;
+    } else if position_id <= 6 {
+        board[1][position_id - 4] = mark;
+    } else {
+        board[2][position_id - 7] = mark;
+    }
 }
 
 fn write_screen(board: [[&str; 3]; 3]) {
