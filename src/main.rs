9717bc87c2a84f869036f4c9dafe2df7cd767e0c
Luiz Henrique Silva Sampaio <luizhsampaio07@gmail.com>
2023-07-15 11:23:13 -0300

    Added write_screen function
    Changes to be committed:
    	modified:   src/main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,21 @@
 fn main() {
-    println!("Hello, world!");
+    let board = [["1", "2", "3"], ["4", "5", "6"], ["7", "8", "9"]];
+    write_screen(board);
+}
+
+fn write_screen(board: [[&str; 3]; 3]) {
+    println!("{}", "-".repeat(13));
+    for row in board {
+        let mut column_number = 1;
+        let row_size = row.len();
+        for column in row {
+            if row_size != column_number {
+                print!("| {} ", column);
+            } else {
+                println!("| {} |", column);
+            }
+            column_number += 1;
+        }
+        println!("{}", "-".repeat(13));
+    }
 }
